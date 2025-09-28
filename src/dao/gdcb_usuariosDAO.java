5362db7735e3ca6aede5bd31eb69c4fb6c635c11
dominique31415 <giovanna.bernazzolli@estudante.ifms.edu.br.com>
2025-09-28 20:24:59 -0300

    Fiz o DAO, colei o AbstractDAO E O Hibernate.DAO do código que fizemos aula passada e colei aqui, no meu simpático dao. Fiz o gdcb_usuariosDAO baseado no código que fizemos em sala de aula

---
+++ src/dao/gdcb_usuariosDAO.java
@@ -0,0 +1,67 @@
+package dao;
+
+import bean.GdcbUsuarios;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author u05084603105
+ */
+public class gdcb_usuariosDAO extends AbstractDAO {
+
+    @Override
+    public void insert(Object object) {
+        session.beginTransaction();
+        session.save(object);
+        session.getTransaction().commit();;
+    }
+
+    @Override
+    public void update(Object object) {
+        session.beginTransaction();
+        session.flush();
+        session.clear();
+        session.update(object);
+        session.getTransaction().commit();;
+    }
+
+    @Override
+    public void delete(Object object) {
+        session.beginTransaction();
+        session.flush();
+        session.clear();
+        session.delete(object);
+        session.getTransaction().commit();;
+    }
+
+    @Override
+    public Object list(int codigo) {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(GdcbUsuarios.class);
+        criteria.add(Restrictions.eq("gdcbIdusuarios", codigo));
+        List lista = criteria.list();
+        session.getTransaction().commit();
+        return lista;
+    }
+
+    @Override
+    public Object listAll() {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(GdcbUsuarios.class);
+        List lista = criteria.list();
+        session.getTransaction().commit();
+        return lista;
+    }
+
+    public static void main(String[] args) {
+        gdcb_usuariosDAO usuariosDAO = new gdcb_usuariosDAO();
+        usuariosDAO.listAll();
+    }
+}
